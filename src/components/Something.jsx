37e29cbf048def08f5668b26d0f0e7b7caab846d
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-26 19:08:25 -0600

    chore: Refactor Something component to improve readability and performance

--- src/components/Something.jsx
+++ src/components/Something.jsx
@@ -4,15 +4,15 @@ const Something = () => {
   const { data, error, isError, isLoading } = useSomething()
   console.log(data)
   return (
-    <>
+    <div style={{ backgroundColor: 'black', minWidth: '100vw', minHeight: '100vh', color: 'white' }}>
       { (isLoading) && <p>Loading...</p> }
       { (isError) && <p>Error: {error.message}</p> }
       { (data) && (data.map((item) => (
-        <div>
+        <div key={item?.id}>
           <h1>{item?.name}</h1>
         </div>
       ))) }
-    </>
+    </div>
   )
 }
 
