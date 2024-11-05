e93034b44d784f84311e5ffc2078be26137aae0f
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-11-05 07:22:29 -0600

    add example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import planet from './assets/planet.webp';
+import space from './assets/space.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Parallax pages={4}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${space})`,
+            backgroundSize: 'cover',
+          }}
+        />
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${planet})`,
+            backgroundSize: 'cover',
+          }}
+        />
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
         >
-          Learn React
-        </a>
-      </header>
+          <h2 style={{ textAlign: 'center' }}>Welcome to Parallax example</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+        >
+          <h3 style={{ textAlign: 'center' }}>Web development is fun!</h3>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
